da8be4b8ba58f954a797bc9975e09abf7ded69ae
404Foundxxx <gmarte303@gmail.com>
2025-06-14 18:42:27 -0400

    se agregaron constructor, getters y setters a la clase Profesor.java

--- src/model/Profesor.java
+++ src/model/Profesor.java
@@ -1,5 +1,40 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Profesor {
-    
+    private String id;
+    private String nombre;
+    private ArrayList<String> materias;
+
+    public Profesor(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+        this.materias = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<String> getMaterias() {
+        return materias;
+    }
+
+    public void setMaterias(ArrayList<String> materias) {
+        this.materias = materias;
+    }
+
 }
