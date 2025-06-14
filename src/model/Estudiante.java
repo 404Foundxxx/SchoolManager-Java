284b8a4c40d6932a0090468f3a63735b3b703d48
404Foundxxx <gmarte303@gmail.com>
2025-06-14 18:44:41 -0400

    se agregaron constructor, getters y setters a la clase Estudiante.java y se implementó método toString() para mostrar datos del estudiante

--- src/model/Estudiante.java
+++ src/model/Estudiante.java
@@ -1,4 +1,67 @@
 package model;
-public class Estudiante {
 
+import java.util.ArrayList;
+
+public class Estudiante {
+    private String id;
+    private String nombre;
+    private int edad;
+    private Curso curso;
+    private ArrayList<Double> calificaciones;
+
+    public Estudiante(String id, String nombre, int edad, Curso curso) {
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.curso = curso;
+        this.calificaciones = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public ArrayList<Double> getCalificaciones() {
+        return calificaciones;
+    }
+
+    public void setCalificaciones(ArrayList<Double> calificaciones) {
+        this.calificaciones = calificaciones;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                "\nNombre: " + nombre +
+                "\nEdad: " + edad +
+                "\nCurso: " + curso.getNombre();
+    }
 }
