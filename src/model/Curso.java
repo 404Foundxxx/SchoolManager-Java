84de9461177adff55e15488fdb2f8e1973a2c01d
404Foundxxx <gmarte303@gmail.com>
2025-06-14 18:46:35 -0400

    se agregaron constructor, getters y setters a la clase Curso.java

--- src/model/Curso.java
+++ src/model/Curso.java
@@ -1,5 +1,41 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Curso {
+    private String nombre;
+    private Profesor profesorAsignado;
+    private ArrayList<Estudiante> estudiantes;
+
+    public Curso(String nombre, Profesor profesorAsignado) {
+        this.nombre = nombre;
+        this.profesorAsignado = profesorAsignado;
+        this.estudiantes = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Profesor getProfesorAsignado() {
+        return profesorAsignado;
+    }
+
+    public void setProfesorAsignado(Profesor profesorAsignado) {
+        this.profesorAsignado = profesorAsignado;
+    }
+
+    public ArrayList<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(ArrayList<Estudiante> estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+
     
 }
