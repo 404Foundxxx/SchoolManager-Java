e905d73843fc0836eec4869b9bcca695c22960ab
404Foundxxx <gmarte303@gmail.com>
2025-06-15 16:47:54 -0400

    se reorganizo la clase profesor y se añadió el método "mostrarInformacion" y "asignarCurso"

--- src/model/Profesor.java
+++ src/model/Profesor.java
@@ -5,36 +5,57 @@ import java.util.ArrayList;
 public class Profesor {
     private String id;
     private String nombre;
-    private ArrayList<String> materias;
+    private ArrayList<Curso> cursos;
 
     public Profesor(String id, String nombre) {
         this.id = id;
         this.nombre = nombre;
-        this.materias = new ArrayList<>();
+        this.cursos = new ArrayList<>();
     }
 
+    public void asignarCurso(Curso curso) {
+        cursos.add(curso);
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("====== Datos del maestro ======");
+        System.out.println(
+                "ID: " + getId() +
+                        "\nNombre: " + getNombre() +
+                        "\nCursos que imparte: ");
+        if (cursos.isEmpty()) {
+            System.out.println("  Ninguan");
+        } else {
+            for (Curso curso : cursos) {
+                System.out.println("  - " + curso.getNombre());
+            }
+        }
+    }
+
+    // Getters
     public String getId() {
         return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getNombre() {
         return nombre;
     }
 
+    public ArrayList<Curso> getMaterias() {
+        return cursos;
+    }
+
+    // Setters
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public ArrayList<String> getMaterias() {
-        return materias;
-    }
-
-    public void setMaterias(ArrayList<String> materias) {
-        this.materias = materias;
+    public void setMaterias(ArrayList<Curso> cursos) {
+        this.cursos = cursos;
     }
 
 }
