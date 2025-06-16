8432ab4ec92e73c186e570d4802a2f74ed9aa50a
404Foundxxx <gmarte303@gmail.com>
2025-06-15 23:56:53 -0400

    se agregaron las instancias de las clases del packages "service"

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,12 +2,17 @@ package app;
 
 import java.util.Scanner;
 
+import service.GestionCalificaciones;
+import service.GestionCursos;
+import service.GestionEstudiantes;
+import service.GestionProfesores;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("====== SISTEMA DE GESTIÓN ESCOLAR ======");
+        int opcion;
+        do {
+            System.out.println("====== SISTEMA DE GESTION ESCOLAR ======");
             System.out.println("1. Gestión de Estudiantes");
             System.out.println("2. Gestión de Profesores");
             System.out.println("3. Gestión de Cursos");
@@ -15,17 +20,25 @@ public class Main {
             System.out.println("5. Reportes");
             System.out.println("6. Salir");
 
-            System.out.println("Seleccione una opción:");
-            int opción = scanner.nextInt();
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
 
-            switch (opción) {
+            switch (opcion) {
                 case 1:
+                    GestionEstudiantes gestionEstudiantes = new GestionEstudiantes();
+                    gestionEstudiantes.subMenu();
                     break;
                 case 2:
+                    GestionProfesores gestionProfesores = new GestionProfesores();
+                    gestionProfesores.subMenu();
                     break;
                 case 3:
+                    GestionCursos gestionCursos = new GestionCursos();
+                    gestionCursos.subMenu();
                     break;
                 case 4:
+                    GestionCalificaciones gestionCalificaciones = new GestionCalificaciones();
+                    gestionCalificaciones.subMenu();
                     break;
                 case 5:
                     break;
@@ -36,8 +49,7 @@ public class Main {
                     System.out.println("Error: Opcion invalida, intente de nuevo.");
                     break;
             }
-            scanner.close();
-        }
+        } while (opcion != 7);
     }
 
 }
