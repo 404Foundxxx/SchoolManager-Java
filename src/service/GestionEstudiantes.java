c0767017b9ed65d20ff2a421c71f8afadc8dc748
404Foundxxx <gmarte303@gmail.com>
2025-06-18 23:53:17 -0400

    se completo el submenu de la clase GestionEstudiantes implemtando las siguientes opciones: "buscarEstudiantePorId", "editarEstudiante" y "liminarEstudiante". en un futuro se requiere una optimización, por ahora todo bien

--- src/service/GestionEstudiantes.java
+++ src/service/GestionEstudiantes.java
@@ -9,6 +9,7 @@ public class GestionEstudiantes {
     private ArrayList<Estudiante> estudiantes;
     private String id, nombre;
     private int edad, opcion;
+    private boolean encontrado;
 
     public GestionEstudiantes() {
         this.estudiantes = new ArrayList<>();
@@ -40,46 +41,191 @@ public class GestionEstudiantes {
      * GestionEstudiantes
      */
     public void listarEstudiantes() {
-        System.out.println("====== Estudiantes ======");
+        int contador = 1;
         for (Estudiante e : estudiantes) {
-            e.mostrarInformacion();
+            if (e.equals(e)) {
+                System.out.println("====== Estudiante: " + (contador++) + " ======");
+                e.mostrarInformacion();
+
+            }
         }
+    }
+
+    /*
+     * Método que busca a un estudiante especifico mediante su ID
+     */
+    public void buscarEstudiantePorId() {
+        scanner.nextLine();
+        System.out.println("====== Buscar Estudiante ======\n");
+        System.out.print("Ingrese el ID del estudiante: ");
+        id = scanner.nextLine();
+        encontrado = false;
+        for (Estudiante e : estudiantes) {
+            if (e.getId().equals(id)) {
+                e.mostrarInformacion();
+                encontrado = true;
+                break;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("El estudiante no esta resgistrado.");
+        }
+    }
+
+    /*
+     * Metodo que edita datos de un estudiante X que el usuario desee editar
+     */
+    public void editarEstudiante() {
+        do {
+            scanner.nextLine();
+            System.out.println("====== Editar Estudiante ======\n");
+            System.out.println("Que desea etidar del estudiante?");
+            System.out.println("1. Nombre");
+            System.out.println("2. ID");
+            System.out.println("3. Edad");
+            System.out.println("0. Atras");
+            System.out.println("Selecciones una opcion: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    scanner.nextLine();
+                    System.out.print("Ingrese el ID del estudiante a editar: ");
+                    id = scanner.nextLine();
+                    encontrado = false;
+                    for (Estudiante e : estudiantes) {
+                        if (e.getId().equals(id)) {
+                            e.mostrarInformacion();
+                            encontrado = true;
+                            System.out.print("Ingrese el nuevo nombre: ");
+                            nombre = scanner.nextLine();
+                            e.setNombre(nombre);
+                            System.out.println("El nombre se a editado correctamente.");
+                            break;
+                        }
+                    }
+                    if (!encontrado) {
+                        System.out.println("El estudiante no esta resgistrado.");
+                    }
+                    break;
+                case 2:
+                    scanner.nextLine();
+                    System.out.print("Ingrese el ID del estudiante a editar: ");
+                    id = scanner.nextLine();
+                    encontrado = false;
+                    for (Estudiante e : estudiantes) {
+                        if (e.getId().equals(id)) {
+                            e.mostrarInformacion();
+                            encontrado = true;
+                            System.out.print("Ingrese el nuevo ID: ");
+                            id = scanner.nextLine();
+                            e.setId(id);
+                            System.out.println("El ID se a editado correctamente.");
+                            break;
+                        }
+                    }
+                    if (!encontrado) {
+                        System.out.println("El estudiante no esta resgistrado.");
+                    }
+                    break;
+                case 3:
+                    scanner.nextLine();
+                    System.out.print("Ingrese el ID del estudiante a editar: ");
+                    id = scanner.nextLine();
+                    encontrado = false;
+                    for (Estudiante e : estudiantes) {
+                        if (e.getId().equals(id)) {
+                            e.mostrarInformacion();
+                            encontrado = true;
+                            System.out.print("Ingrese la nueva edad: ");
+                            edad = scanner.nextInt();
+                            e.setEdad(edad);
+                            System.out.println("La edad se a editado correctamente.");
+                            break;
+                        }
+                    }
+                    if (!encontrado) {
+                        System.out.println("El estudiante no esta resgistrado.");
+                    }
+                    break;
+                case 0:
+                    System.out.println("Volviendo a atras...");
+                    break;
+
+                default:
+                    break;
+            }
+        } while (opcion != 0);
+    }
+
+    /*
+     * Este metodo elimina estudiante del array desde consola, segun elija el
+     * usuario
+     */
+    public void eliminarEstudiante() {
+        scanner.nextLine();
+        System.out.println("====== Eliminar Estudiante ======\n");
+        System.out.print("Ingrese el ID del estudiante a elimiar: ");
+        id = scanner.nextLine();
+        encontrado = false;
+        for (Estudiante e : estudiantes) {
+            if (e.getId().equals(id)) {
+                e.mostrarInformacion();
+                encontrado = true;
+                System.out.println("Seguro que desea eliminar? [S/N]: ");
+                char deccicion = scanner.next().charAt(0);
+                if (deccicion != 'S') {
+                    System.out.println("Volviendo atras...");
+                    break;
+                } else {
+                    estudiantes.remove(e);
+                    System.out.println("El estudiante se a eliminado correctamente.");
+                    break;
+                }
+            }
+        }
+        if (!encontrado) {
+            System.out.println("El estudiante no esta resgistrado.");
+        }
+
     }
 
     // Metodo principal de la clase GestionEstudiantes como subMenu
     public void subMenu() {
-        System.out.println("====== Gestion de Estudiates ======");
-        System.out.println("1. Agregar Estudiates");
-        System.out.println("2. Listar Estudiates");
-        System.out.println("3. Buscar Estudiates");
-        System.out.println("4. Editar Estudiates");
-        System.out.println("5. Eliminar Estudiates");
-        System.out.println("0. Volver al menu principal");
+        do {
+            System.out.println("====== Gestion de Estudiates ======");
+            System.out.println("1. Agregar Estudiates");
+            System.out.println("2. Listar Estudiates");
+            System.out.println("3. Buscar Estudiates");
+            System.out.println("4. Editar Estudiates");
+            System.out.println("5. Eliminar Estudiates");
+            System.out.println("0. Volver al menu principal");
 
-        System.out.print("Seleccione una opcion: ");
-        opcion = scanner.nextInt();
+            System.out.print("Seleccione una opcion: ");
+            opcion = scanner.nextInt();
 
-        switch (opcion) {
-            case 1:
-                agregarEstudiante();
-                break;
-            case 2:
-                listarEstudiantes();
-                break;
-            case 3:
-                // Logica para buscar estudiante
-                break;
-            case 4:
-                // Logica para editar estudiante
-                break;
-            case 5:
-                // Logica para eliminar estudiante
-                break;
-            case 0:
-                System.out.println("Volviendo al menu principal...");
-                break;
-            default:
-                System.out.println("Opcion no valida, intente nuevamente.");
-        }
+            switch (opcion) {
+                case 1:
+                    agregarEstudiante();
+                    break;
+                case 2:
+                    listarEstudiantes();
+                    break;
+                case 3:
+                    buscarEstudiantePorId();
+                    break;
+                case 4:
+                    editarEstudiante();
+                    break;
+                case 5:
+                    eliminarEstudiante();
+                    break;
+                case 0:
+                    System.out.println("Volviendo al menu principal...");
+                    break;
+                default:
+                    System.out.println("Opcion no valida, intente nuevamente.");
+            }
+        } while (opcion != 0);
     }
 }
