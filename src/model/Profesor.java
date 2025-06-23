e744a42bac5888107e81db92ee209d360baf3823
404Foundxxx <gmarte303@gmail.com>
2025-06-22 22:21:54 -0400

    implementa Serializable, se añadieron métodos para gestionar materias y método toString()

--- src/model/Profesor.java
+++ src/model/Profesor.java
@@ -1,38 +1,29 @@
 package model;
 
-public class Profesor {
+import java.io.Serializable;
+import java.util.ArrayList;
 
-    // Atributos
+public class Profesor implements Serializable {
     private String id;
     private String nombre;
+    private ArrayList<String> materias; // Materias que imparte
 
-    // Metodo Construtro de la clase Profesor
     public Profesor(String id, String nombre) {
         this.id = id;
         this.nombre = nombre;
+        this.materias = new ArrayList<>();
     }
 
-    public void mostrarInformacion() {
-        System.out.println(
-                "ID: " + getId() +
-                        "\nNombre: " + getNombre() + "\n");
-    }
+    // Getters y Setters
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public ArrayList<String> getMaterias() { return materias; }
+    public void agregarMateria(String materia) { this.materias.add(materia); }
 
-    // Getters
-    public String getId() {
-        return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    // Setters
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Nombre: " + nombre + ", Materias: " + materias;
     }
 }
