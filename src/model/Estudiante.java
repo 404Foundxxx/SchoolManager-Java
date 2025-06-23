f1fb08c6007dbf240595be22c1a6057bea7ab636
404Foundxxx <gmarte303@gmail.com>
2025-06-22 22:23:13 -0400

    mejorar clase Estudiante con Serializable, curso asociado y manejo de calificaciones

--- src/model/Estudiante.java
+++ src/model/Estudiante.java
@@ -1,66 +1,38 @@
 package model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Estudiante {
-
-    // Atributos
+public class Estudiante implements Serializable {
     private String id;
     private String nombre;
     private int edad;
+    private Curso curso; // Asociación con la clase Curso
     private ArrayList<Double> calificaciones;
 
-    // Metodo constructor de la clase Estudiante sin parametros
-    public Estudiante() {
-    }
-
-    // Metodo constructor de la clase Estudiante con parametros
-    public Estudiante(String id, String nombre, int edad) {
+    public Estudiante(String id, String nombre, int edad, Curso curso) {
         this.id = id;
         this.nombre = nombre;
         this.edad = edad;
+        this.curso = curso;
         this.calificaciones = new ArrayList<>();
     }
 
-    public void mostrarInformacion() {
-        System.out.println(
-                "ID: " + getId() +
-                        "\nNombre: " + getNombre() +
-                        "\nEdad: " + getEdad() + "\n");
-    }
+    // Getters y Setters
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public int getEdad() { return edad; }
+    public void setEdad(int edad) { this.edad = edad; }
+    public Curso getCurso() { return curso; }
+    public void setCurso(Curso curso) { this.curso = curso; }
+    public ArrayList<Double> getCalificaciones() { return calificaciones; }
+    public void agregarCalificacion(double calificacion) { this.calificaciones.add(calificacion); }
 
-    // Getters
-    public String getId() {
-        return id;
+    @Override
+    public String toString() {
+        String cursoNombre = (curso != null) ? curso.getNombre() : "No asignado";
+        return "ID: " + id + ", Nombre: " + nombre + ", Edad: " + edad + ", Curso: " + cursoNombre + ", Calificaciones: " + calificaciones;
     }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public ArrayList<Double> getCalificaciones() {
-        return calificaciones;
-    }
-
-    // Setters
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public void setCalificaciones(ArrayList<Double> calificaciones) {
-        this.calificaciones = calificaciones;
-    }
-
 }
